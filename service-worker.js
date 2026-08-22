e1a7a2a542ed9b89a99d5b0eea09a1a60bf594ef
proyectosguevara46-blip <proyectosguevara46@gmail.com>
2026-08-22 00:38:07 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,39 @@
-const CACHE_NAME = "marcaqr-v1";
+const CACHE_NAME = "marcaqr-v2";
 
 const archivos = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icono-marcaqr.png"
+  "./icono-marcaqr.png",
+  "./icono-marcaqr-192.png",
+  "./icono-marcaqr-512.png"
 ];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(archivos);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(nombres => {
+      return Promise.all(
+        nombres
+          .filter(nombre => nombre !== CACHE_NAME)
+          .map(nombre => caches.delete(nombre))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
